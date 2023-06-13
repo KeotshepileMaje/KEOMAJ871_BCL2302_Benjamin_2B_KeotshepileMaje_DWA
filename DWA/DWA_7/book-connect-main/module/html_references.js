7fbb7d2e28b37e9df6d34a0cc9ee89b1d893c977
keotshepileMaje <keotshepilemaje@gmail.com>
2023-06-13 12:13:10 +0200

    Changed file location

---
+++ DWA/DWA_7/book-connect-main/module/html_references.js
@@ -0,0 +1,88 @@
+/**
+ * @typedef {Object} HtmlReference
+ * @property {HTMLElement} headerSearch - Reference to the header search element.
+ * @property {HTMLElement} headerSettings - Reference to the header settings element.
+ * @property {HTMLElement} listItems - Reference to the list items element.
+ * @property {HTMLElement} listMessage - Reference to the list message element.
+ * @property {HTMLElement} listButton - Reference to the list button element.
+ * @property {HTMLElement} listActive - Reference to the list active element.
+ * @property {HTMLElement} listBlur - Reference to the list blur element.
+ * @property {HTMLElement} lisImage - Reference to the list image element.
+ * @property {HTMLElement} listTitle - Reference to the list title element.
+ * @property {HTMLElement} listSubtitle - Reference to the list subtitle element.
+ * @property {HTMLElement} listDescription - Reference to the list description element.
+ * @property {HTMLElement} listClose - Reference to the list close element.
+ * @property {HTMLElement} searchOverlay - Reference to the search overlay element.
+ * @property {HTMLElement} searchForm - Reference to the search form element.
+ * @property {HTMLElement} searchTitle - Reference to the search title element.
+ * @property {HTMLElement} searchGenres - Reference to the search genres element.
+ * @property {HTMLElement} searchAuthors - Reference to the search authors element.
+ * @property {HTMLElement} searchCancel - Reference to the search cancel element.
+ * @property {HTMLElement} settingsOverlay - Reference to the settings overlay element.
+ * @property {HTMLElement} settingsForm - Reference to the settings form element.
+ * @property {HTMLElement} settingsTheme - Reference to the settings theme element.
+ * @property {HTMLElement} settingsCancel - Reference to the settings cancel element.
+ */
+
+/**
+ * Contains references to various HTML elements selected by their data attributes.
+ * @type {HtmlReference}
+ */
+ 
+ const htmlReference = {
+  
+    headerSearch: document.querySelector('[data-header-search]'),
+    headerSettings: document.querySelector('[data-header-settings]'),
+
+    listItems: document.querySelector('[data-list-items]'),
+    listMessage: document.querySelector('[data-list-message]'),
+    listButton: document.querySelector('[data-list-button]'),
+    listActive: document.querySelector('[data-list-active]'),
+    listBlur: document.querySelector('[data-list-blur]'),
+    listImage: document.querySelector('[data-list-image'),
+    listTitle: document.querySelector('[data-list-title]'),
+    listSubtitle: document.querySelector('[data-list-subtitle]'),
+    listDescription: document.querySelector('[data-list-description]'),
+    listClose: document.querySelector('[data-list-close]'),
+
+    searchOverlay: document.querySelector('[data-search-overlay]'),
+    searchForm: document.querySelector('[data-search-form]'),
+    searchTitle: document.querySelector('[data-search-title]'),
+    searchGenres: document.querySelector('[data-search-genres]'),
+    searchAuthors: document.querySelector('[data-search-authors]'),
+    searchCancel: document.querySelector('[data-search-cancel]'),
+
+    settingsOverlay: document.querySelector('[data-settings-overlay]'),
+    settingsForm: document.querySelector('[data-settings-form]'),
+    settingsTheme: document.querySelector('[data-settings-theme]'),
+    settingsCancel: document.querySelector('[data-settings-cancel]'),
+}
+
+export const {
+    headerSearch,
+    headerSettings,
+
+    listItems,
+    listMessage,
+    listButton,
+    listActive,
+    listBlur,
+    listImage,
+    listTitle,
+    listSubtitle,
+    listDescription,
+    listClose,
+    
+    searchOverlay,
+    searchForm,
+    searchTitle,
+    searchGenres,
+    searchAuthors,
+    searchCancel,
+    
+    settingsOverlay,
+    settingsForm,
+    settingsTheme,
+    settingsCancel
+  } = htmlReference;
+  
---
+++ DWA/DWA_7/book-connect-main/scripts.js
@@ -0,0 +1,236 @@
+import { books, authors, genres, BOOKS_PER_PAGE } from './module/data.js'
+
+import {
+        headerSearch,
+        headerSettings,
+
+        listItems,
+        listMessage,
+        listButton,
+        listActive,
+        listBlur,
+        listImage,
+        listTitle,
+        listSubtitle,
+        listDescription,
+        listClose,
+
+        searchOverlay,
+        searchForm,
+        searchTitle,
+        searchGenres,
+        searchAuthors,
+        searchCancel,
+        settingsOverlay,
+        settingsForm,
+        settingsTheme,
+        settingsCancel
+    } from './module/html_references.js'
+
+searchCancel.addEventListener('click', () => {
+    searchOverlay.open = false
+})
+
+settingsCancel.addEventListener('click', () => {
+    settingsOverlay.open = false
+})
+
+headerSearch.addEventListener('click', () => {
+    searchOverlay.open = true 
+    searchTitle.focus()
+})
+
+headerSettings.addEventListener('click', () => {
+    settingsOverlay.open = true 
+})
+
+listClose.addEventListener('click', () => {
+    listActive.open = false
+})   
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    settingsTheme.value = 'night'
+    document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
+    document.documentElement.style.setProperty('--color-light', '10, 10, 20');
+} else {
+    settingsTheme.value = 'day'
+    document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
+    document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+}
+
+
+settingsForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const { theme } = Object.fromEntries(formData)
+
+    if (theme === 'night') {
+        document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
+        document.documentElement.style.setProperty('--color-light', '10, 10, 20');
+    } else {
+        document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
+        document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+    }
+    
+    settingsOverlay.open = false
+})
+
+
+let page = 0;
+let matches = books
+
+const fragment = document.createDocumentFragment()
+/**
+ * This function is used to display the 36 books at the time
+ */
+let displayBooks = () => {
+    for (const { author, id, image, title } of matches.slice(page * BOOKS_PER_PAGE, (page + 1) * BOOKS_PER_PAGE)) {
+        const element = document.createElement('button')
+        element.classList = 'preview'
+        element.setAttribute('data-preview', id)
+
+        element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `
+
+        fragment.appendChild(element)
+    }
+
+    listItems.appendChild(fragment)
+}
+
+//Add the first 36 books
+displayBooks()
+page += 1
+
+listButton.innerText = `Show more (${books.length - BOOKS_PER_PAGE})`
+listButton.disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 0
+
+const showMoreButton = () => {
+
+    listButton.innerHTML = `
+        <span>Show more</span>
+        <span class="list__remaining"> (${(matches.length - (page * BOOKS_PER_PAGE)) > 0 ? (matches.length - (page * BOOKS_PER_PAGE)) : 0})</span>
+    `
+}
+listButton.addEventListener('click', () => {
+    displayBooks()
+    page += 1
+    showMoreButton()
+})
+showMoreButton()
+
+listItems.addEventListener('click', (event) => {
+    const pathArray = Array.from(event.path || event.composedPath())
+    let active = null
+
+    for (const node of pathArray) {
+        if (active) break
+
+        if (node?.dataset?.preview) {
+            let result = null
+    
+            for (const singleBook of books) {
+                if (result) break;
+                if (singleBook.id === node?.dataset?.preview) result = singleBook
+            } 
+        
+            active = result
+        }
+    }
+  
+    if (active) {
+        listActive.open = true
+        listBlur.src = active.image
+        listImage.src = active.image
+        listTitle.innerText = active.title
+        listSubtitle.innerText = `${authors[active.author]} (${new Date(active.published).getFullYear()})`
+        listDescription.innerText = active.description
+    }
+})
+
+const optionFragment = document.createDocumentFragment()
+
+/**
+ * This function create options on the form for users to choose which books
+ * to read.
+ * @param {string} - the parameter should only be 'genres' or 'authors'
+ */
+
+let createOption = (param) => {
+    const firstElement = document.createElement('option')
+    firstElement.value = 'any'
+    firstElement.innerText = 'All Genres'
+    optionFragment.appendChild(firstElement)
+
+    for (const [id, name] of Object.entries(param)) {
+        const element = document.createElement('option')
+        element.value = id
+        element.innerText = name
+        optionFragment.appendChild(element)
+    }
+
+}
+
+//Call the function to create options for genres
+createOption(genres)
+searchGenres.appendChild(optionFragment)
+
+//Call the function to create options for authors
+createOption(authors)
+searchAuthors.appendChild(optionFragment)
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const filters = Object.fromEntries(formData)
+    const result = []
+
+    for (const book of books) {
+        let genreMatch = filters.genre === 'any'
+
+        for (const singleGenre of book.genres) {
+            if (genreMatch) break;
+            if (singleGenre === filters.genre) { genreMatch = true }
+        }
+
+        if (
+            (filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase())) && 
+            (filters.author === 'any' || book.author === filters.author) && 
+            genreMatch
+        ) {
+            result.push(book)
+        }
+    }
+   
+    page = 0;
+    matches = result
+
+    if (result.length < 1) {
+        listMessage.classList.add('list__message_show')
+    } else {
+        listMessage.classList.remove('list__message_show')
+    }
+
+    listItems.innerHTML = ''
+    displayBooks()
+    listItems.appendChild(fragment)
+
+    page += 1
+
+    listButton.disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 0
+
+    showMoreButton()
+
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    searchOverlay.open = false
+})
