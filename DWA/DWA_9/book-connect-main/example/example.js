a9fc362d099722e4938a954254b7e3ba7795772d
keotshepileMaje <keotshepilemaje@gmail.com>
2023-06-22 08:32:30 +0200

    example files

--- DWA/DWA_9/book-connect-main/example/example.js
+++ DWA/DWA_9/book-connect-main/example/example.js
@@ -1,29 +1,54 @@
-class CreateHTML extends HTMLElement {
-    constructor (id, image, title, author) {
-        super();
 
-        this.attachShadow({mode: 'open'});
 
-        const element = document.createElement('button');
-        element.classList = 'preview';
-        element.setAttribute('data-preview', id);
-    
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${author}</div>
-            </div>
-        `;
 
-        this.shadowRoot.appendChild(element);
-    }
-    
-}
 
-customElements.define( 'create-html', CreateHTML)
 
+
+
+
+
+// class CreateHTML extends HTMLElement {
+//   constructor() {
+//     super();
+
+//     this.attachShadow({ mode: 'open' });
+
+//     const element = document.createElement('button');
+//     element.classList.add('preview');
+//     // Add the correct id variable value or remove the attribute if not needed
+//     element.setAttribute('data-preview', 'YOUR_ID_HERE');
+
+//     element.innerHTML = `
+//       <img class="preview__image" src="https://picsum.photos/200" />
+//       <div class="preview__info">
+//         <h3 class="preview__title">YOUR_TITLE_HERE</h3>
+//         <div class="preview__author">YOUR_AUTHOR_HERE</div>
+//       </div>
+//     `;
+
+//     this.shadowRoot.appendChild(element);
+//   }
+// }
+
+// customElements.define('create-html', CreateHTML);
+
+
+// const createHTML = (id, image, title, author) => {
+//   const element = document.createElement('button')
+//   element.classList = 'preview'
+//   element.setAttribute('data-preview', id)
+
+//   element.innerHTML = `
+//       <img
+//           class="preview__image"
+//           src="${image}"
+//       />
+      
+//       <div class="preview__info">
+//           <h3 class="preview__title">${title}</h3>
+//           <div class="preview__author">${authors[author]}</div>
+//       </div>
+//   `
+
+//   return element
+// }
