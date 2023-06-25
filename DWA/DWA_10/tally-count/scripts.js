cca52e48c93a455faf00ee2c2bc04bec732552b8
keotshepileMaje <keotshepilemaje@gmail.com>
2023-06-25 22:20:24 +0200

    improve the code

--- DWA/DWA_10/tally-count/scripts.js
+++ DWA/DWA_10/tally-count/scripts.js
@@ -5,6 +5,7 @@ const STEP_AMOUNT = 1;
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="substract"]')
 const add = document.querySelector('[data-key="add"]')
+const reset = document.querySelector('.controls > sl-button')
 
 const subtractHandler = () => {
     const newValue = parseInt(number.value) - STEP_AMOUNT
@@ -16,6 +17,7 @@ const subtractHandler = () => {
     if (newValue <= MIN_NUMBER) {
         subtract.disabled = true  
         subtract.style.background = 'red'
+        throw new Error ("The number can't be greater than 5")
     }
     if (newValue < 5) {
         add.style.background = ''
@@ -33,12 +35,19 @@ const addHandler = () =>{
     if (newValue>=MAX_NUMBER) {
         add.disabled = true
     }
+    if (newValue >= 4) {
+        add.style.background = 'red'
+        throw new Error ("The number can't be less than 5")
+    }
     if (newValue > -5) {
         subtract.style.background = ''
     }
-    if (newValue >= 4) {
-        add.style.background = 'red'
-    }
+}
+
+const resetHandler = () => {
+    number.value = 0
+    alert("The counter has been reset")
 }
 subtract.addEventListener('click', subtractHandler);
 add.addEventListener('click', addHandler);
+reset.addEventListener('click', resetHandler)
